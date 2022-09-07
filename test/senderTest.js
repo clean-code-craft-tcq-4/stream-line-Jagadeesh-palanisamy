62a7e3968dbcd7c32d5cb06ebbc0491f825e676a
Jagadeesh Palanisamy <palanisamy.jagadeesh@in.bosch.com>
2022-09-07 18:06:24 +0530

    added testcases

--- test/senderTest.js
+++ test/senderTest.js
@@ -1,8 +1,6 @@
 const {expect} = require('chai');
 const sender = require('../sender');
 
-
-
 describe('randomValueGenerator', () => {
     it('should exists', () => {
       expect(sender.randomValueGenerator).to.exist;
@@ -10,5 +8,22 @@ describe('randomValueGenerator', () => {
     it('should return randomValue', () => {
         let list=sender.randomValueGenerator(0,10,5);
      expect(list.length).to.deep.equals(5);
+    });
+  });
+
+  describe('temperatureSensor', () => {
+    it('should exists', () => {
+      expect(sender.temperatureSensor).to.exist;
+    });
+    it('should return temperature Sensor exect value count', () => {
+        let list=sender.temperatureSensor(0,50,10);
+     expect(list.length).to.deep.equals(10);
+    });
+    it('should return temperature Sensor random value between the min and max value given', () => {
+        let list=sender.temperatureSensor(0,50,10);
+        let minValue = Math.min(...list);
+        let maxValue = Math.max(...list);
+        expect(minValue).toBeGreaterThan(0)
+        expect(maxValue).toBeLessThan(50)
     });
   });
