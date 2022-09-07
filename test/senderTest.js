0972d974d4729392ad5beb4fc28b8f86c791bbdd
Jagadeesh Palanisamy <palanisamy.jagadeesh@in.bosch.com>
2022-09-07 18:27:21 +0530

    added test case

--- test/senderTest.js
+++ test/senderTest.js
@@ -1,29 +1,52 @@
-const {expect} = require('chai');
+const { expect } = require('chai');
 const sender = require('../sender');
 
 describe('randomValueGenerator', () => {
     it('should exists', () => {
-      expect(sender.randomValueGenerator).to.exist;
+        expect(sender.randomValueGenerator).to.exist;
     });
     it('should return randomValue', () => {
-        let list=sender.randomValueGenerator(0,10,5);
-     expect(list.length).to.deep.equals(5);
+        let list = sender.randomValueGenerator(0, 10, 5);
+        expect(list.length).to.deep.equals(5);
     });
-  });
+});
 
-  describe('temperatureSensor', () => {
+describe('temperatureSensor', () => {
     it('should exists', () => {
-      expect(sender.temperatureSensor).to.exist;
+        expect(sender.temperatureSensor).to.exist;
     });
     it('should return temperature Sensor exect value count', () => {
-        let list=sender.temperatureSensor(0,50,10);
-     expect(list.length).to.deep.equals(10);
+        let list = sender.temperatureSensor(0, 50, 10);
+        expect(list.length).to.deep.equals(10);
     });
     it('should return temperature Sensor random value between the min and max value given', () => {
-        let list=sender.temperatureSensor(0,50,10);
+        let list = sender.temperatureSensor(0, 50, 10);
         let minValue = Math.min(...list);
         let maxValue = Math.max(...list);
         expect(minValue).to.be.above(0)
         expect(maxValue).to.be.below(50)
     });
-  });
+});
+
+describe('socSensor', () => {
+    it('should exists', () => {
+        expect(sender.socSensor).to.exist;
+    });
+    it('should return soc Sensor exect value count', () => {
+        let list = sender.socSensor(0, 30, 20);
+        expect(list.length).to.deep.equals(10);
+    });
+    it('should return soc Sensor random value between the min and max value given', () => {
+        let list = sender.socSensor(10, 70, 10);
+        let minValue = Math.min(...list);
+        let maxValue = Math.max(...list);
+        expect(minValue).to.be.above(10)
+        expect(maxValue).to.be.below(70)
+    });
+});
+
+describe('sensorStatistics', () => {
+    it('should exists', () => {
+        expect(sender.sensorStatistics).to.exist;
+    });
+});
